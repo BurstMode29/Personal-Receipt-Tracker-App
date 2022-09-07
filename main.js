2280e2bd49492d402d3eb86441dad7cf16c905e2
Rafeeq Weideman <106829867+BurstMode29@users.noreply.github.com>
2022-09-07 15:02:37 +0200

    Add files via upload
    Commit

---
+++ main.js
@@ -0,0 +1,64 @@
+// Menu
+function openMenu() {
+    const btn = document.getElementById("menuContent").style.display = "initial";
+    if (document.getElementById("menuContent").style.display = "intial") {
+      return document.getElementById("Switch").style.display = "none"
+    }   
+}
+
+function closeMenu() {
+    const btn = document.getElementById("menuContent").style.display = "none";
+    if (document.getElementById("menuContent").style.display = "none") {
+      return document.getElementById("Switch").style.display = "initial"
+    }   
+}
+// Menu
+
+// RegForm
+function signUp() {
+    const btn = document.getElementById("Register").style.display = "none";
+    if (document.getElementById("Register").style.display = "none") {
+      return document.getElementById("Login").style.display = "none"
+    }
+}
+
+function Login() {
+  const btn = document.getElementById("Register").style.display = "none";
+  if (document.getElementById("Register").style.display = "none") {
+    return document.getElementById("SignUp").style.display = "none"
+  }
+}
+// RegForm
+
+
+// SignUp Form
+function Submit() {
+    const btn = document.getElementById("SignUp").style.display = "none";
+}
+// SignUp Form
+
+// Login
+function SignIn() {
+  const btn = document.getElementById("Login").style.display = "none";
+}
+
+// LoaderAnimation
+const myTimeout = setTimeout(myLoader, 1000);
+
+function myLoader() {
+  document.getElementById("Loader").style.display = "none";
+}
+// LoaderAnimation
+
+// DocSetup 
+
+  function DocGen() {
+    const btn = document.getElementById("DocSetup").style.display = "initial";
+    if (document.getElementById("DocSetup").style.display = "initial") {
+      return document.getElementById("Main").style.display = "none";
+    }
+  }
+
+
+// DocSetup
+
